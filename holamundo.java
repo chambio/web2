2720a52c582ed70b893c4357a6f7f987a5aeb043
luis fernando chambi orosco <lchambio@unsa.edu.pe>
2022-04-28 22:20:17 -0500

    agregando mensaje de bienvenida al cusro de programacion web2

--- holamundo.java
+++ holamundo.java
@@ -2,5 +2,6 @@ public class holamundo{
 public static void main (String args[]){
 //hola mundo
 System.out.println("¡hola mundo!");
+System.out.println("bienvenidos al curso de programacion web 2");
   }
 }
